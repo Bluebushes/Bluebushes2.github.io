3b671ff68c40240f7dfc392c2472031b9145456b
Brendan Yang <puffspat@gmail.com>
2018-06-07 04:50:37 +0000

    NinthCommit

--- FinalProject/Calcu.js
+++ FinalProject/Calcu.js
@@ -1,4 +1,48 @@
-var x = 10;
-var y = 10;
+var x = 0;
+var op = '+';
+var y = 0;
 
-document.getElementById("calcResult").innerHTML = x + y;
+
+if ($("#1").on("click")){
+    x = x + 1;
+} else{
+    
+}
+
+if ($("#2").on("click")){
+    x = x + 2;
+} else{
+    
+}
+
+
+
+
+
+if (op == '+'){
+   function calcResultPrint(){
+    document.getElementById("calcResult").innerHTML = x + y;
+   }
+   $("#enter").on("click",calcResultPrint);
+}
+
+if (op == '-'){
+    function calcResultPrint(){
+        document.getElementById("calcResult").innerHTML = x - y;
+    }
+    $("#enter").on("click",calcResultPrint);
+}
+
+if (op == '*'){
+    function calcResultPrint(){
+        document.getElementById("calcResult").innerHTML = x * y;
+    }
+    $("#enter").on("click",calcResultPrint);
+}
+
+if (op == '/'){
+    function calcResultPrint(){
+        document.getElementById("calcResult").innerHTML = x / y;
+    }
+    $("#enter").on("click",calcResultPrint);
+}
