5f0bd7b101038716ceb2d89050d4d1b198bbf610
Brendan Yang <puffspat@gmail.com>
2018-06-07 16:48:53 +0000

    iForgotHowManyCommits

--- FinalProject/NumbG.js
+++ FinalProject/NumbG.js
@@ -1,9 +1,14 @@
 var cont = 0;
+var maxNum = 0;
+
+function setmaxNum(){
+    maxNum = document.getElementById("maxNum").value;
+}
 
 function printNum(){
-if (cont < 1){
-    cont = 1;
-    var i = Math.floor(Math.random() * 100) + 1;
+    if (cont < 1){
+        cont = 1;
+        var i = Math.floor(Math.random() * maxNum) + 1;
 }
     document.getElementById("randNum").innerHTML = i;
 
