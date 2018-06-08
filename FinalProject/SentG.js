47750922ad9530f7cad98ad5639115eccd42bae1
Brendan Yang <puffspat@gmail.com>
2018-06-08 13:49:04 +0000

    FINALEDIT @

--- FinalProject/SentG.js
+++ FinalProject/SentG.js
@@ -1,17 +1,14 @@
-var noun1 = ["Brendan", "Kevin", "GlobalHack", "A dog", "A cat", "BTS"];
-var verb = ["ran to", "walked to", "flew to", "ate", "kicked", "washed"];
-var noun2 = ["a park", "a home", "a person", "some food", "you", "Kevin Tan"];
+var noun1 = ["A singer", "Jung Kook", "Mukund", "Reese", "Brendan", "Kevin", "GlobalHack", "A dog", "A cat", "BTS", "A homeless person", "A fly", "The United States", "China", "Kim Jung Un", "Trump", "Lil Tay", "Yanny", "Laurel", "George Washington", "Barack Obama"];
+var verb = ["ran to", "walked to", "flew to", "ate", "drank", "sled down", "kicked", "washed", "sold", "became", "ruined", "cleaned", "destroyed", "bought", "made", "spoke to", "taped", "stapled", "burned", "painted", "sang about"];
+var noun2 = ["Summer Break", "Phani", "a park", "a home", "a person", "some food", "you", "a school", "a camp", "a waterslide", "a painting", "water", "a laptop", "a chair", "an office", "a parent", "guitars", "sodas", "schools", "soup kitchens", "Earth"];
 var cont = 0;
-
-
-
 
 function printSent(){
 if (cont < 1){
     cont = 1;
-    var i = Math.floor(Math.random() * 6);
-    var x = Math.floor(Math.random() * 6);
-    var y = Math.floor(Math.random() * 6);
+    var i = Math.floor(Math.random() * 21);
+    var x = Math.floor(Math.random() * 21);
+    var y = Math.floor(Math.random() * 21);
 }
     document.getElementById("randSent").innerHTML = noun1[i] + " " + verb[x] + " " + noun2[y];
 
